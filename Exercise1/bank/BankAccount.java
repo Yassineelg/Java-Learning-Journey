b011a5b890d3ccb2f584b63cfcd94f3a9694843d
Yassine EL GHERRABI <yassineelgherrabi@gmail.com>
2023-07-27 15:05:13 +0200

    feat: Add password hashing and salting to BankAccount creation
    In this commit, I've implemented password hashing with PBKDF2 and salt generation to securely store user passwords.
    
    - Added imports for necessary security classes and interfaces, including NoSuchAlgorithmException, SecureRandom, InvalidKeySpecException, KeySpec, SecretKeyFactory, and PBEKeySpec.
    - Implemented the generateSalt() method, which generates a random 16-byte salt using SecureRandom.
    - Implemented the hashPassword() method, which takes the user's password and the generated salt as input. It then hashes the password using the PBKDF2 algorithm with 65536 iterations and a key length of 128 bits (SHA-1 HMAC), and returns the resulting hashed password as a Base64-encoded string.
    - Modified the createAccount() method to use the new password hashing and salting process. Now, before writing the user's information to the "accounts.txt" file, it generates a salt, hashes the password using the hashPassword() method, and then encodes the salt as a Base64 string.
    
    With these changes, the BankAccount class now provides a more secure way to handle user passwords, protecting them from potential security breaches.
    
    Refs: #Exercise1

--- Exercise1/bank/BankAccount.java
+++ Exercise1/bank/BankAccount.java
@@ -10,6 +10,13 @@ package bank;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Random;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.util.Base64;
 
 public class BankAccount {
     private int accountNumber;
@@ -53,13 +60,35 @@ public class BankAccount {
         int accountNumber = random.nextInt(90000000) + 10000000;
         double balance = 0.00;
 
+        byte[] salt = generateSalt();
+        String hashedPassword = hashPassword(password, salt);
+        String saltString = Base64.getEncoder().encodeToString(salt);
+
         try {
             FileWriter writer = new FileWriter("accounts.txt", true);
-            writer.write(accountNumber + "," + accountHolder + "," + password + "," + balance + "\n");
+            writer.write(accountNumber + "," + accountHolder + "," + hashedPassword + "," + saltString + "," + balance + "\n");
             writer.close();
             System.out.println("Account created successfully! Your account number is: " + accountNumber);
         } catch (IOException e) {
             System.out.println("An error occurred while saving the account information.");
+        }
+    }
+
+    private static byte[] generateSalt() {
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+        return salt;
+    }
+
+    private static String hashPassword(String password, byte[] salt) {
+        try {
+            KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 128);
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            byte[] hash = factory.generateSecret(spec).getEncoded();
+            return Base64.getEncoder().encodeToString(hash);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new RuntimeException("Error while hashing password", e);
         }
     }
 }
